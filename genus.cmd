27d9b06fb5ebd7f7e7323142faf1489b3511c907
8304_2 <8304_2@vlsi305.vlsi.polymtl.ca>
2018-11-19 16:01:20 -0500

    add lab1 files + new files in Lab2

---
+++ genus.cmd
@@ -0,0 +1,36 @@
+# Cadence Genus(TM) Synthesis Solution, Version 17.10-p007_1, built Aug  3 2017
+
+# Date: Mon Nov 05 15:30:28 2018
+# Host: vlsi304 (x86_64 w/Linux 3.10.0-862.14.4.el7.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Core(TM) i7 CPU 960 @ 3.20GHz 8192KB)
+# OS:   Scientific Linux release 7.5 (Nitrogen)
+
+ls
+cd Lab2
+ls
+cd scripts
+ls
+config
+source config.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+source shifter.tcl
+ls
+ls vf
+ls fv
+source shifter.tcl
+ls fv
+rm fv/*
+rm fv/
+rm -f fv
+rm -r fv
+ls
+source shifter.tcl
+ls fv
