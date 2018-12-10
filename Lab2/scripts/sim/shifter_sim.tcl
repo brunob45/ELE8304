bf89df1c97bd71b0cf0d6afb624f9a40e0b09962
8304_2 <8304_2@vlsi310.vlsi.polymtl.ca>
2018-12-10 18:06:31 -0500

    scripts sim

---
+++ Lab2/scripts/sim/shifter_sim.tcl
@@ -0,0 +1,22 @@
+set lib     "~/Labs/Lab2/simulation/beh"
+set srcD    "~/Labs/Lab2/sources/hdl"
+set top     "shifter"
+set tb      "${top}_tb"
+set modules [list]
+set simtime "2000"
+# Librairie de travail
+if { [file exists $lib] } {
+file delete -force $lib
+}
+vlib $lib
+# Compilation
+foreach module $modules {
+vcom -work $lib $srcD/$module.vhd
+}
+vcom -work $lib $srcD/$top.vhd
+
+vcom -2008 -work $lib $srcD/$tb.vhd
+# Simulation
+vsim $work.$tb
+do ${top}_wave.do
+run $simtime ns
