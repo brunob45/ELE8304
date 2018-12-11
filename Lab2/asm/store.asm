c0adc559cd21ea2fca33047fe1555cababe826fd
8304_2 <8304_2@vlsi310.vlsi.polymtl.ca>
2018-12-11 03:52:37 -0500

    commit final

---
+++ Lab2/asm/store.asm
@@ -0,0 +1,17 @@
+start:	
+	addi  x4  x0  0x0C
+	addi  x2  x0  6
+;;	nop
+	nop
+	nop
+	sw    x2  x4  0
+	nop
+	nop	
+	nop
+	lw    x2  x4  0
+	nop
+	nop
+	nop
+	nop
+	nop
+	
--- Lab2/genus.cmd
+++ Lab2/genus.cmd
@@ -1,22 +1,15 @@
 # Cadence Genus(TM) Synthesis Solution, Version 17.10-p007_1, built Aug  3 2017
 
-# Date: Mon Nov 05 15:50:26 2018
-# Host: vlsi304 (x86_64 w/Linux 3.10.0-862.14.4.el7.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Core(TM) i7 CPU 960 @ 3.20GHz 8192KB)
-# OS:   Scientific Linux release 7.5 (Nitrogen)
+# Date: Mon Dec 10 12:48:06 2018
+# Host: vlsi310 (x86_64 w/Linux 3.10.0-957.1.3.el7.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Core(TM) i7 CPU 960 @ 3.20GHz 8192KB)
+# OS:   Scientific Linux release 7.6 (Nitrogen)
 
-ls
 cd scripts
-ls
-rm -r fv
+source setup.csh
+source config.csh
 ls
 source config.tcl
-source shifter.tcl
-ls fv
-help elaborate
-read_hdl -vhdl shifter.vhd
-elaborate rv_shifter
-help check_design
-source shifter.tcl
-rm fv -r
-ls
-source shifter.tcl
+clear
+clear
+source elab/core_elaborate.tcl
+exit
